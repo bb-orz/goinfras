08e9786d0fc9bf419f701a3e76c49c5e5e99fda4
gofuncchan <gofuncchan@163.com>
2020-11-30 01:54:15 +0800

    fix starter

--- example/simple/app/component.go
+++ example/simple/app/component.go
@@ -1,18 +1,12 @@
 package main
 
 import (
+	_ "GoWebScaffold/example/simple/apis" // 初始化时自动注册apis层的所有接口
 	"GoWebScaffold/infras"
-	"GoWebScaffold/infras/cron"
+	"GoWebScaffold/infras/ginger"
 	"GoWebScaffold/infras/hook"
 	"GoWebScaffold/infras/logger"
-	"GoWebScaffold/infras/mq/natsMq"
-	"GoWebScaffold/infras/mq/redisPubSub"
-	"GoWebScaffold/infras/oauth"
-	"GoWebScaffold/infras/oss/aliyunOss"
-	"GoWebScaffold/infras/oss/qiniuOss"
-	"GoWebScaffold/infras/store/mongoStore"
-	"GoWebScaffold/infras/store/redisStore"
-	"GoWebScaffold/infras/store/sqlbuilderStore"
+	"GoWebScaffold/infras/validate"
 	"io"
 	"os"
 )
@@ -27,24 +21,29 @@ func registerComponent() {
 	writers := []io.Writer{file}
 	// 注册zap日志记录启动器
 	infras.Register(&logger.Starter{Writers: writers})
-	// 注册mongodb启动器
-	infras.Register(&mongoStore.Starter{})
-	// 注册mysql启动器
-	infras.Register(&sqlbuilderStore.Starter{})
-	// 注册Redis连接池
-	infras.Register(&redisStore.Starter{})
-	// 注册Oss
-	infras.Register(&aliyunOss.Starter{})
-	infras.Register(&qiniuOss.Starter{})
-	// 注册Mq
-	infras.Register(&redisPubSub.Starter{})
-	infras.Register(&natsMq.Starter{})
-	// 注册Oauth Manager
-	infras.Register(&oauth.Starter{})
-	// 注册Cron定时任务
-	infras.Register(&cron.Starter{})
 	// 注册hook
 	infras.Register(&hook.Starter{})
+	// 注册mongodb启动器
+	// infras.Register(new(mongoStore.Starter))
+	// 注册mysql启动器
+	// infras.Register(new(sqlBuilderStore.Starter{})
+	// 注册Redis连接池
+	// infras.Register(new(redisStore.Starter{})
+	// 注册Oss
+	// infras.Register(new(aliyunOss.Starter{})
+	// infras.Register(new(qiniuOss.Starter{})
+	// 注册Mq
+	// infras.Register(new(redisPubSub.Starter{})
+	// infras.Register(new(natsMq.Starter{})
+	// 注册Oauth Manager
+	// infras.Register(new(oauth.Starter))
+	// 注册Cron定时任务
+	// infras.Register(new(cron.Starter))
+	// 注册gin web 服务
+	infras.Register(&ginger.Starter{})
+	// 注册验证器
+	infras.Register(&validate.Starter{})
+
 	// 对资源组件启动器进行排序
 	infras.SortStarters()
 }
--- example/simple/app/flag.go
+++ example/simple/app/flag.go
@@ -13,7 +13,7 @@ var (
 // 绑定命令行参数
 func bindingFlag() {
 	// 启动时获取命令行flag参数
-	flag.StringVar(&flagConfigFile, "f", "", "Config file,like: ./build/config.yaml")
+	flag.StringVar(&flagConfigFile, "f", "", "Config file,like: ../config/config.yaml")
 	flag.StringVar(&flagRemoteProvider, "T", "", "Remote K/V config system provider，support etcd/consul")
 	flag.StringVar(&flagRemoteEndpoint, "E", "", "Remote K/V config system endpoint，etcd requires http://ip:port  consul requires ip:port")
 	flag.StringVar(&flagRemotePath, "P", "", "Remote K/V config path，path is the path in the k/v store to retrieve configuration,like: /configs/myapp.json")
