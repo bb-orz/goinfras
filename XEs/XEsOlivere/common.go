7a268d90c0c47f8d293568c77157b8a0d0f3efec
gofuncchan <gofuncchan@163.com>
2020-12-10 23:41:00 +0800

    XEsOlivere V1.0

--- XEs/XEsOlivere/common.go
+++ XEs/XEsOlivere/common.go
@@ -1,16 +1,64 @@
 package XEsOlivere
 
 import (
+	"context"
+	"fmt"
 	elasticv7 "github.com/olivere/elastic/v7"
+	"strconv"
 )
 
 type EsCommon struct {
 	client *elasticv7.Client
 }
 
-// TODO 编写一些es client 的通用操作
+// 编写一些es client 的通用操作
 
-func (c *EsCommon) Status() error {
+// 查看某文档是否存在,给定文档ID查询
+func (c *EsCommon) IsDocExists(id int, index string) (bool, error) {
+	var err error
+	exist, err := c.client.Exists().Index(index).Id(strconv.Itoa(id)).Do(context.Background())
+	if err != nil {
+		return false, err
+	}
+	if !exist {
+		return false, nil
+	}
+	return true, nil
+}
 
-	return nil
+// 获取文档
+func (c *EsCommon) GetDoc(id int, index string) (*elasticv7.GetResult, error) {
+	esResponse, err := c.client.Get().Index(index).Id(strconv.Itoa(id)).Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return esResponse, nil
+}
+
+// 添加文档
+func (c *EsCommon) AddDoc(id int, doc string, index string) (*elasticv7.IndexResponse, error) {
+	rsp, err := c.client.Index().Index(index).Id(strconv.Itoa(id)).BodyJson(doc).Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
+
+// 更新文档
+func (c *EsCommon) UpdateDoc(updateField *map[string]interface{}, id int, index string) (*elasticv7.UpdateResponse, error) {
+	rsp, err := c.client.Update().Index(index).Id(strconv.Itoa(id)).Doc(updateField).Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return rsp, nil
+}
+
+// 删除文档
+func (c *EsCommon) DeleteDoc(id int, index string) (*elasticv7.DeleteResponse, error) {
+	rsp, err := c.client.Delete().Index(index).Id(strconv.Itoa(id)).Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
 }
