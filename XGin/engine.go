231da8be6ff20f39766803394241e800dcda9a09
gofuncchan <gofuncchan@163.com>
2020-12-12 20:30:48 +0800

    XGin V1.0

--- XGin/engine.go
+++ XGin/engine.go
@@ -2,7 +2,10 @@ package XGin
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
+
+var ginEngine *gin.Engine
 
 func NewGinEngine(cfg *Config, middlewares ...gin.HandlerFunc) *gin.Engine {
 	var engine *gin.Engine
@@ -13,4 +16,15 @@ func NewGinEngine(cfg *Config, middlewares ...gin.HandlerFunc) *gin.Engine {
 	engine.Use(middlewares...)
 
 	return engine
+}
+
+func CreateDefaultEngine(config *Config, logger *zap.Logger) {
+	// 1.配置gin中间件
+	middlewares := make([]gin.HandlerFunc, 0)
+	middlewares = append(middlewares, ZapLoggerMiddleware(logger), ZapRecoveryMiddleware(logger, false))
+
+	if config == nil {
+		config = DefaultConfig()
+	}
+	ginEngine = NewGinEngine(config, middlewares...)
 }
--- XGin/xgin.go
+++ XGin/xgin.go
@@ -2,21 +2,7 @@ package XGin
 
 import (
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 )
-
-var ginEngine *gin.Engine
-
-func CreateDefaultEngine(config *Config, logger *zap.Logger) {
-	// 1.配置gin中间件
-	middlewares := make([]gin.HandlerFunc, 0)
-	middlewares = append(middlewares, ZapLoggerMiddleware(logger), ZapRecoveryMiddleware(logger, false))
-
-	if config == nil {
-		config = DefaultConfig()
-	}
-	ginEngine = NewGinEngine(config, middlewares...)
-}
 
 // 资源组件实例调用
 func XEngine() *gin.Engine {
