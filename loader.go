8732f796697346cf9cd6fd853f069186a3515f1c
gofuncchan <gofuncchan@163.com>
2020-12-17 16:58:20 +0800

    fix viper loader

--- loader.go
+++ loader.go
@@ -6,6 +6,7 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 /**
@@ -48,9 +49,10 @@ func LoadViperConfigFromFile(viperCfg *viper.Viper, configFilePath string) error
  * @param remoteEndpoint  远程配置主机节点
  * @param remotePath	  远程配置键节点
  * @param remoteType	  因为在字节流中没有文件扩展名，所以这里需要设置下类型。支持的扩展名有 "json", "toml", "yaml", "yml", "properties", "props", "prop", "env", "dotenv"
+ * @param etcdWatchDuration		etcd watch 监听时间间隔
  * @return error
  */
-func LoadViperConfigFromRemote(viperCfg *viper.Viper, remoteProvider, remoteEndpoint, remotePath, remoteType string) error {
+func LoadViperConfigFromRemote(viperCfg *viper.Viper, remoteProvider, remoteEndpoint, remotePath, remoteType string, etcdWatchDuration time.Duration) error {
 	var err error
 	switch remoteProvider {
 	case "etcd", "consul":
@@ -61,6 +63,41 @@ func LoadViperConfigFromRemote(viperCfg *viper.Viper, remoteProvider, remoteEndp
 		if err = viper.ReadRemoteConfig(); err != nil {
 			return err
 		}
+
+		// unmarshal config
+		var rawVals interface{}
+		err := viperCfg.Unmarshal(&rawVals)
+		ErrorHandler(err)
+
+		// open a goroutine to watch remote changes forever
+		go func() {
+			var err error
+			for {
+				time.Sleep(time.Second * etcdWatchDuration) // delay after each request
+
+				// currently, only tested with etcd support
+				err = viperCfg.WatchRemoteConfig()
+				if err != nil {
+					ErrorHandler(err)
+					continue
+				}
+
+				// unmarshal new config into our runtime config struct. you can also use channel
+				// to implement a signal to notify the system of the changes
+				err = viperCfg.Unmarshal(&rawVals)
+				ErrorHandler(err)
+			}
+		}()
+
+	case "consult":
+		if err = viperCfg.AddRemoteProvider(remoteProvider, remoteEndpoint, remotePath); err != nil {
+			return err
+		}
+		viperCfg.SetConfigType(remoteType)
+		if err = viper.ReadRemoteConfig(); err != nil {
+			return err
+		}
+
 	default:
 		return errors.New("Only Support etcd/consul K/V System. ")
 	}
@@ -76,25 +113,32 @@ func LoadViperConfigFromRemote(viperCfg *viper.Viper, remoteProvider, remoteEndp
  * @param envKeys			绑定特定环境变量
  * @param envAllowEmpty		是否允许读取空值的环境变量
  * @param envAutomatic		是否载入所有环境变量，如设置Prefix，则只筛选有前缀的载入
- * @param envKeyReplacer	键名字符替换器，用于排除一些前缀符合但不需要的环境变量
+ * @param envKeyReplacer	键名字符替换器，常用语替换键名连接符
  * @return error
  */
 func LoadViperConfigFromEnv(viperCfg *viper.Viper, envPrefix string, envKeys []string, envAllowEmpty, envAutomatic bool, envKeyReplacer *strings.Replacer) error {
 	viperCfg.AllowEmptyEnv(envAllowEmpty) // 是否允许环境变量为空值,默认为false
-	if envKeyReplacer != nil {
-		viperCfg.SetEnvKeyReplacer(envKeyReplacer) // 替换一些不需要的变量
-	}
+
 	if envPrefix != "" {
 		viperCfg.SetEnvPrefix(envPrefix) // 添加需加载系统环境变量的前缀字符串
 	}
-	if len(envKeys) > 0 {
-		err := viperCfg.BindEnv(envKeys...) // 绑定特定环境变量值到viper
-		if err != nil {
-			return err
+
+	if envKeys != nil {
+		for _, key := range envKeys {
+			err := viperCfg.BindEnv(key) // 绑定特定环境变量值到viper
+			if err != nil {
+				return err
+			}
 		}
 	}
+
+	if envKeyReplacer != nil {
+		viperCfg.SetEnvKeyReplacer(envKeyReplacer) // 替换键名的字符
+	}
+
 	if envAutomatic {
 		viperCfg.AutomaticEnv() // 自动载入所有环境变量，如设置SetEnvPrefix，则加载有特定前缀的所有环境变量
 	}
+
 	return nil
 }
