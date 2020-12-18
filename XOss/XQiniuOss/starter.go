4a650095bc0e3c01089728c3db9bc41f01d7cc2a
gofuncchan <gofuncchan@163.com>
2020-12-19 01:50:34 +0800

    QiniuOss Starter Logger

--- XOss/XQiniuOss/starter.go
+++ XOss/XQiniuOss/starter.go
@@ -26,28 +26,27 @@ func (s *starter) Init(sctx *goinfras.StarterContext) {
 	viper := sctx.Configs()
 	if viper != nil {
 		err = viper.UnmarshalKey("QiniuOss", &define)
-		goinfras.ErrorHandler(err)
+		sctx.PassWarning(s.Name(), goinfras.StepInit, err)
 	}
 	if define == nil {
 		define = DefaultConfig()
 	}
 	s.cfg = define
-	fmt.Printf("XQiniuOss Starter Init: [Config] %v \n", *define)
+	sctx.Logger().SDebug(s.Name(), goinfras.StepInit, fmt.Sprintf("Config: %v \n", *define))
 }
 
 func (s *starter) Setup(sctx *goinfras.StarterContext) {
 	qiniuOssClient = NewQnClient(s.cfg)
-	sctx.Logger().Info("QiniuOss Setup Successful!")
+	sctx.Logger().SInfo(s.Name(), goinfras.StepSetup, fmt.Sprintf("Qiniu Oss Client Setuped! \n"))
 }
 
 func (s *starter) Check(sctx *goinfras.StarterContext) bool {
 	err := goinfras.Check(qiniuOssClient)
-	if err != nil {
-		sctx.Logger().Error(fmt.Sprintf("[%s Starter]: QiniuOss Client Setup Fail!", s.Name()))
-		return false
+	if sctx.PassError(s.Name(), goinfras.StepCheck, err) {
+		sctx.Logger().SInfo(s.Name(), goinfras.StepCheck, fmt.Sprintf("Qiniu Oss Client Setup Successful! \n"))
+		return true
 	}
-	sctx.Logger().Info(fmt.Sprintf("[%s Starter]: QiniuOss Client Setup Successful!", s.Name()))
-	return true
+	return false
 }
 
 // 设置启动组级别
