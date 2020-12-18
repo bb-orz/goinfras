82ede7f5bfb8e0bd894372c70540a47cfc453b83
gofuncchan <gofuncchan@163.com>
2020-12-19 01:34:11 +0800

    XRedisPubSub Starter Logger

--- XMQ/XRedisPubSub/run_test.go
+++ XMQ/XRedisPubSub/run_test.go
@@ -3,6 +3,7 @@ package XRedisPubSub
 import (
 	"fmt"
 	"github.com/bb-orz/goinfras"
+	"github.com/bb-orz/goinfras/XLogger"
 	. "github.com/smartystreets/goconvey/convey"
 	"go.uber.org/zap"
 	"testing"
@@ -16,19 +17,19 @@ const (
 
 func TestRedisSubscriber(t *testing.T) {
 	Convey("TestRedisSubscriber", t, func() {
-		logger, err := zap.NewDevelopment()
-		So(err, ShouldBeNil)
-		CreateDefaultPool(nil, logger)
+		XLogger.CreateDefaultLogger(nil)
+
+		CreateDefaultPool(nil)
 		recSubMsgFuncs := make(map[string]RecSubMsgFunc)
 		// ChannelName1 订阅频道消息的处理函数
 		recSubMsgFuncs[ChannelName1] = func(channel string, msg interface{}) error {
-			logger.Info("Receive Message:", zap.String("channel", channel), zap.Any("message", msg))
+			XLogger.XCommon().Info("Receive Message:", zap.String("channel", channel), zap.Any("message", msg))
 			fmt.Println(msg)
 			return nil
 		}
 		// ChannelName2 订阅频道消息的处理函数
 		recSubMsgFuncs[ChannelName2] = func(channel string, msg interface{}) error {
-			logger.Info("Receive Message:", zap.String("channel", channel), zap.Any("message", msg))
+			XLogger.XCommon().Info("Receive Message:", zap.String("channel", channel), zap.Any("message", msg))
 			fmt.Println(msg)
 			return nil
 		}
@@ -42,7 +43,7 @@ func TestRedisSubscriber(t *testing.T) {
 			unSubCh <- ChannelName1
 			unSubCh <- ChannelName2
 		}()
-		err = XRedisSubscriber(logger).Subscribe(recSubMsgFuncs, unSubCh)
+		err := XRedisSubscriber().Subscribe(recSubMsgFuncs, unSubCh)
 		So(err, ShouldBeNil)
 
 	})
@@ -50,10 +51,11 @@ func TestRedisSubscriber(t *testing.T) {
 
 func TestPublisher(t *testing.T) {
 	Convey("TestRedisSubscriber", t, func() {
-		logger, err := zap.NewDevelopment()
-		So(err, ShouldBeNil)
-		CreateDefaultPool(nil, logger)
-		publisher := XRedisPublisher(logger)
+		XLogger.CreateDefaultLogger(nil)
+
+		CreateDefaultPool(nil)
+		publisher := XRedisPublisher()
+		var err error
 		err = publisher.Publish(ChannelName1, "this a message from TestPublisher To ChannelName1")
 		So(err, ShouldBeNil)
 		err = publisher.Publish(ChannelName2, "this a message from TestPublisher To ChannelName2")
@@ -63,22 +65,12 @@ func TestPublisher(t *testing.T) {
 
 func TestStarter(t *testing.T) {
 	Convey("Test XRedisPubSub Starter", t, func() {
-
-		s := NewStarter()
-		logger, err := zap.NewDevelopment()
-		So(err, ShouldBeNil)
+		logger := goinfras.NewCommandLineStarterLogger()
 		sctx := goinfras.CreateDefaultStarterContext(nil, logger)
+		s := NewStarter()
 		s.Init(sctx)
-		Println("Starter Init Successful!")
-
 		s.Setup(sctx)
-		Println("Starter Setup Successful!")
+		s.Check(sctx)
 		s.Start(sctx)
-		Println("Starter Start Successful!")
-		if s.Check(sctx) {
-			Println("Component Check Successful!")
-		} else {
-			Println("Component Check Fail!")
-		}
 	})
 }
