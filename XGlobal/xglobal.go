d96aab240558b4a261dc6b5480c049ac64abef61
gofuncchan <gofuncchan@163.com>
2020-12-22 14:59:46 +0800

    modified XGlobal

--- XGlobal/xglobal.go
+++ XGlobal/xglobal.go
@@ -1,9 +1,55 @@
 package XGlobal
 
 // 全局配置
-var global Config
+const (
+	Env      = "Env"      // 允许环境：dev、testing、product
+	Host     = "Host"     // 主机地址
+	Endpoint = "Endpoint" // 节点
+	AppName  = "AppName"  // 应用名
+	Version  = "Version"  // 应用版本
+)
 
-// 资源组件实例调用
-func GConfig() *Config {
-	return &global
+// Usage: env := XGlobal.G().Get("your global config key")
+func G() Global {
+	return _g
+}
+
+func GetEnv() string {
+	s := _g.Get(Env)
+	if s == nil {
+		return "undefined"
+	}
+	return s.(string)
+}
+
+func GetHost() string {
+	s := _g.Get(Host)
+	if s == nil {
+		return "undefined"
+	}
+	return s.(string)
+}
+
+func GetEndpoint() string {
+	s := _g.Get(Endpoint)
+	if s == nil {
+		return "undefined"
+	}
+	return s.(string)
+}
+
+func GetAppName() string {
+	s := _g.Get(AppName)
+	if s == nil {
+		return "undefined"
+	}
+	return s.(string)
+}
+
+func GetVersion() string {
+	s := _g.Get(Version)
+	if s == nil {
+		return "undefined"
+	}
+	return s.(string)
 }
