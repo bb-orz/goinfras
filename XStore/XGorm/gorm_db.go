0a5250001b48585597e267f4d0c4e51dab42dbac
gofuncchan <gofuncchan@163.com>
2020-12-28 22:52:15 +0800

    fix

--- XStore/XGorm/gorm_db.go
+++ XStore/XGorm/gorm_db.go
@@ -2,6 +2,7 @@ package XGorm
 
 import (
 	"database/sql"
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
@@ -26,6 +27,7 @@ func NewORMDb(config *Config) (*gorm.DB, error) {
 	// 先创建一个*sql.DB 连接池
 	sqlDBPool, err = newSqlDB(config)
 
+	fmt.Println(config.Dialect)
 	switch config.Dialect {
 	case "mysql":
 		db, err = gorm.Open(mysql.New(mysql.Config{
@@ -36,14 +38,12 @@ func NewORMDb(config *Config) (*gorm.DB, error) {
 			DontSupportRenameColumn:   config.DontSupportRenameColumn,   // 是否可以重命名列，MySQL 8和MariaDB之前不支持重命名列
 			SkipInitializeWithVersion: config.SkipInitializeWithVersion, // 是否基于当前MySQL版本自动配置
 		}), &gorm.Config{})
-
 	case "postgres":
 		// source : "host=myhost user=gorm dbname=gorm sslmode=disable password=mypassword"
 		db, err = gorm.Open(postgres.New(postgres.Config{
 			Conn:                 sqlDBPool,
 			PreferSimpleProtocol: true, // 是否禁用隐式 prepared 语法
 		}), &gorm.Config{})
-
 	default:
 		db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 	}
